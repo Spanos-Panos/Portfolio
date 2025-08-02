7b91efdef4f79cbb5eef52610eed02741da85c64
Spanos Panos <panosqwe632@gmail.com>
2025-08-02 09:32:52 +0300

    Removes all the files

--- Assets/js/secretwindow-interop.js
+++
@@ -1,6 +0,0 @@
-
-export function registerSecretWindowInterop(dotnetRef) {
-    window.addEventListener('show-secret-window', () => {
-        if (dotnetRef) dotnetRef.invokeMethodAsync('ShowSecretWindow');
-    });
-}
